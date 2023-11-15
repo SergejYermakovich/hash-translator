c4f5d0bfdec737614c8f31d2efbf70f99652a1f6
Sergey Ermakovich <sergu9848@gmail.com>
2023-11-16 00:18:10 +0300

    user controller update

--- authorization-service/src/main/java/tt/auth/mapper/UserMapper.java
+++ authorization-service/src/main/java/tt/auth/mapper/UserMapper.java
@@ -4,8 +4,12 @@ import org.mapstruct.Mapper;
 import tt.auth.dto.UserDto;
 import tt.auth.model.User;
 
+import java.util.List;
+
 @Mapper
 public interface UserMapper {
 
     User map(UserDto userDto);
+
+    List<UserDto> mapAllUsersToDto(List<User> users);
 }
--- authorization-service/src/main/java/tt/auth/rest/UserController.java
+++ authorization-service/src/main/java/tt/auth/rest/UserController.java
@@ -2,9 +2,15 @@ package tt.auth.rest;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-// убрать *
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import tt.auth.dto.UserDto;
+import tt.auth.mapper.UserMapper;
 import tt.auth.model.User;
 import tt.auth.service.UserService;
 
@@ -16,6 +22,7 @@ import java.util.List;
 public class UserController {
 
     private UserService userService;
+    private UserMapper userMapper;
 
     @PostMapping
     public ResponseEntity<?> createUser(@RequestBody UserDto userDto) {
@@ -30,8 +37,9 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> getUsers() {
-        //mapper mapstruct отдавть дто
-        return ResponseEntity.ok(userService.findAllUsers());
+    public ResponseEntity<List<UserDto>> getUsers() {
+        List<User> users = userService.findAllUsers();
+        List<UserDto> dtoUsers = userMapper.mapAllUsersToDto(users);
+        return ResponseEntity.ok(dtoUsers);
     }
 }
